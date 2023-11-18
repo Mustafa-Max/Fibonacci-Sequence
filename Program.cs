22c2fe1d0b9699c26f725dc69995fdf9255047d6
Mustafa El Rouby <48238118+Mustafa-Max@users.noreply.github.com>
2023-11-19 00:15:51 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        var fibonacciNumbers = new List<Double> { 1, 1 };
+
+        // Generate the next 10 Fibonacci numbers
+        for (Double i = 2; i <= 11; i++)
+        {
+            var previous = fibonacciNumbers[^1];
+            var previous2 = fibonacciNumbers[^2];
+            fibonacciNumbers.Add(previous + previous2);
+        }
+
+        // Print all Fibonacci numbers in the list
+        foreach (var item in fibonacciNumbers)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
