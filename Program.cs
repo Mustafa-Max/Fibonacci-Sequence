d5786e08e62802e440cb3c7598d2b650e7173dd3
Mustafa El Rouby <48238118+Mustafa-Max@users.noreply.github.com>
2023-11-20 00:58:52 +0200

    Update Program.cs
    Add new way by While with foreach

--- Program.cs
+++ Program.cs
@@ -22,3 +22,21 @@ class Program
         }
     }
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+var fibonaccinumbers = new List<int> {1, 1};
+
+// Generate the next 20 Fibonacci numbers
+while (fibonaccinumbers.Count < 20)
+{
+    var number1 = fibonaccinumbers[^1];
+    var number2 = fibonaccinumbers[^2];
+    fibonaccinumbers.Add(number1 + number2);
+}
+
+// Print all Fibonacci numbers in the list
+foreach (var number in fibonaccinumbers)
+{
+    Console.WriteLine(number);
+}
